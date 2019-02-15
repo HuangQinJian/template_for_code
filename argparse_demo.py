5d93634927bcd521f1fd7cd6dfc20775727fe14f
不迷茫，不等待 <huangqjduter@gmail.com>
2019-02-15 13:31:40 +0800

    Add files via upload

---
+++ argparse_demo.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+import json
+import numpy as np
+
+cudnn.benchmark = True
+
+parser = argparse.ArgumentParser(description='Wide Residual Networks')
+# Model options
+parser.add_argument('--model', default='resnet', type=str)
+parser.add_argument('--depth', default=16, type=int)
+parser.add_argument('--width', default=1, type=float)
+parser.add_argument('--dataset', default='CIFAR10', type=str)
+parser.add_argument('--dataroot', default='.', type=str)
+parser.add_argument('--dtype', default='float', type=str)
+parser.add_argument('--groups', default=1, type=int)
+parser.add_argument('--nthread', default=4, type=int)
+parser.add_argument('--seed', default=1, type=int)
+
+# Training options
+parser.add_argument('--batch_size', default=128, type=int)
+parser.add_argument('--lr', default=0.1, type=float)
+parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--weight_decay', default=0.0005, type=float)
+parser.add_argument('--epoch_step', default='[60,120,160]', type=str,
+                    help='json list with epochs to drop lr on')
+parser.add_argument('--lr_decay_ratio', default=0.2, type=float)
+parser.add_argument('--resume', default='', type=str)
+parser.add_argument('--note', default='', type=str)
+
+# Device options
+parser.add_argument('--cuda', action='store_true')
+parser.add_argument('--save', default='', type=str,
+                    help='save parameters and logs in this folder')
+parser.add_argument('--ngpu', default=1, type=int,
+                    help='number of GPUs to use for training')
+parser.add_argument('--gpu_id', default='0', type=str,
+                    help='id(s) for CUDA_VISIBLE_DEVICES')
+
+def main():
+    opt = parser.parse_args()
+    print('parsed options:', vars(opt))
+    epoch_step = json.loads(opt.epoch_step)
+    num_classes = 10 if opt.dataset == 'CIFAR10' else 100
+
+    torch.manual_seed(opt.seed)
+    os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu_id
+
+    if not os.path.exists(opt.save):
+        os.mkdir(opt.save)
+
+if __name__ == '__main__':
+    main()
